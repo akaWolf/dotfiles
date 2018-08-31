5290de4d96f59cd898268feb02183bb0050b36c9
Artjom Vejsel <akawolf0@gmail.com>
2018-08-31 21:09:34 +0300

    qtile config: run xss-lock, which blocks screen using i3lock when going to sleep

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -111,7 +111,7 @@ keys = [
 	# Lock screen
 	Key(
 		[mod], "l",
-		lazy.spawn("i3lock -c000000")
+		lazy.spawn("i3lock --nofork --color=000000")
 	),
 
 	# Backlight (brightness) control
@@ -309,6 +309,8 @@ def startup_once():
 	runInBackground("/usr/lib/polkit-kde-authentication-agent-1", "authentication agent polkit-kde-agent")
 
 	runInBackground("udiskie --smart-tray --use-udisks2", "udisks2 automounter (mount helper)")
+
+	runInBackground("xss-lock -- i3lock --nofork --color=000000", "xss-lock subscribes to the systemd-events suspend, hibernate")
 
 def runInBackground(prog, descr = None):
 	import subprocess
