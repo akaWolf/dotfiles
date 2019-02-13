90c14bbcd9b76a9774b6f1b8aaf63019a06e08c6
Artjom Vejsel <akawolf0@gmail.com>
2019-02-13 15:10:28 +0300

    add Windows7.sh startup file

---
+++ Windows7.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+exec qemu-system-x86_64 \
+	-cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
+	-smp 2 \
+	-enable-kvm \
+	-machine type=q35,accel=kvm \
+	-device intel-iommu \
+	-drive file=/home/akawolf/Windows7.img,format=qcow2,if=virtio \
+	-net nic,model=virtio \
+	-net user,hostname=windowsvm \
+	-soundhw hda \
+	-m 4G \
+	-monitor stdio \
+	-name "Windows 7" \
+	$@
+
+# -drive file=~/Downloads/virtio-win-0.1.141.iso,media=cdrom \
+# -drive file=~/Downloads/en_windows_7_enterprise_with_sp1_x64_dvd_620201.iso,media=cdrom \
+# -vga qxl -spice port=5930,disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent
