407874e475d3f4f1ad7955388759c0667a151148
Artjom Vejsel <akawolf0@gmail.com>
2019-08-07 08:40:24 +0300

    fish config: emacs-specific settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,21 @@ gpg-connect-agent updatestartuptty /bye > /dev/null
 
 # nnn: edit all files in EDITOR
 set -xg NNN_USE_EDITOR 1
+
+# emacs-specific
+if [ -n "$INSIDE_EMACS" ]
+  # dir tracking
+  # see https://emacs.stackexchange.com/a/31446
+  function prompt_AnSiT -e fish_prompt
+    printf "\eAnSiTc %s\n" "$PWD"
+  end
+  # set title to nothing to prevent sending OSC escape sequences
+  # see https://github.com/fish-shell/fish-shell/issues/107
+  function fish_title
+    true
+  end
+  # usefull for TRAMP, disabled for local machine
+  # see https://github.com/emacs-mirror/emacs/blob/220f16cab6c40a1b0df1a5d2101c6602abbc6aae/lisp/term.el#L241
+  #printf "\eAnSiTu %s\n" "$USER"
+  #printf '\033AnSiTh %s\n' "$HOSTNAME"
+end
