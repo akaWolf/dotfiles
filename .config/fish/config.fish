f92f75d79becf0960796eaa04c60a3a8d8622e34
Artjom Vejsel <akawolf0@gmail.com>
2019-11-08 12:11:31 +0300

    fish: setup emacs vterm shell prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,19 +23,27 @@ gpg-connect-agent updatestartuptty /bye > /dev/null
 set -xg NNN_USE_EDITOR 1
 
 # emacs-specific
-if [ -n "$INSIDE_EMACS" ]
+if set -q INSIDE_EMACS
   # dir tracking
-  # see https://emacs.stackexchange.com/a/31446
-  function prompt_AnSiT -e fish_prompt
-    printf "\eAnSiTc %s\n" "$PWD"
+  switch $INSIDE_EMACS
+  case vterm
+  # see https://github.com/akermu/emacs-libvterm#directory-tracking
+    function prompt_vterm -e fish_prompt
+      printf '\e]51;A%s\e\\' "$PWD"
+    end
+  case '*'
+    # see https://emacs.stackexchange.com/a/31446
+    function prompt_AnSiT -e fish_prompt
+      printf "\eAnSiTc %s\n" "$PWD"
+    end
+    # set title to nothing to prevent sending OSC escape sequences
+    # see https://github.com/fish-shell/fish-shell/issues/107
+    function fish_title
+      true
+    end
+    # usefull for TRAMP
+    # see https://github.com/emacs-mirror/emacs/blob/220f16cab6c40a1b0df1a5d2101c6602abbc6aae/lisp/term.el#L241
+    printf "\eAnSiTu %s\n" "$USER"
+    printf '\033AnSiTh %s\n' "$HOSTNAME"
   end
-  # set title to nothing to prevent sending OSC escape sequences
-  # see https://github.com/fish-shell/fish-shell/issues/107
-  function fish_title
-    true
-  end
-  # usefull for TRAMP
-  # see https://github.com/emacs-mirror/emacs/blob/220f16cab6c40a1b0df1a5d2101c6602abbc6aae/lisp/term.el#L241
-  printf "\eAnSiTu %s\n" "$USER"
-  printf '\033AnSiTh %s\n' "$HOSTNAME"
 end
